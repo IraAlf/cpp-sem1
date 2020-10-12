a1d45abf800a7ba29e2ac480115a2747cc0f4df3
Irina Alferova <33869968+IraAlf@users.noreply.github.com>
2020-10-13 01:13:44 +0300

    Update test_ali.cpp

--- project/Alignment/test_ali.cpp
+++ project/Alignment/test_ali.cpp
@@ -43,36 +43,36 @@ elements2[1] = (struct element*)malloc(4 * sizeof ( struct element));
 
 
 el1.value = 1;
-el1.ptr = elements[0];
-elements[0][0] = el1;
+el1.ptr = elements2[0];
+elements2[0][0] = el1;
 
 el1.value = 2;
 el1.ptr = elements2[0] + 1;
-elements[0][1] = el1;
+elements2[0][1] = el1;
 
 el1.value = 0;
 el1.ptr = elements2[0] + 2;
-elements[0][2] = el1;
+elements2[0][2] = el1;
 
 el1.value = 0;
 el1.ptr = elements2[0] + 3;
-elements[0][3] = el1;
+elements2[0][3] = el1;
 
 el1.value = 1;
 el1.ptr = elements2[1];
-elements[1][0] = el1;
+elements2[1][0] = el1;
 
 el1.value = 2;
 el1.ptr = elements2[1] + 1;
-elements[1][1] = el1;
+elements2[1][1] = el1;
 
 el1.value = 3;
 el1.ptr = elements2[1] + 2;
-elements[1][2] = el1;
+elements2[1][2] = el1;
 
 el1.value = 4;
 el1.ptr = elements2[1] + 3;
-elements[1][3] = el1;
+elements2[1][3] = el1;
 
 struct element **element_res = alignment(elements, 2, 4);
 
