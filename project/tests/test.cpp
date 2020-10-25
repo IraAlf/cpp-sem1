591d089e0f9f05d6cbfca2f09acdcc22f8b1a2af
Irina Alferova <33869968+IraAlf@users.noreply.github.com>
2020-10-26 02:11:53 +0300

    Update test.cpp

--- project/tests/test.cpp
+++ project/tests/test.cpp
@@ -18,8 +18,8 @@ extern "C" {
 
 
 TEST(Find_max, test1) {
-char *test_buf = "irina alferova";
-EXPECT_STR_EQ(find_max_word(test_buf), "alferova");
+char test_buf[50] = "irina alferova";
+EXPECT_STR_EQ(find_max_word(test_buf, 14), "alferova");
 }
 
 
@@ -33,7 +33,7 @@ clock_gettime(CLOCK_MONOTONIC, &finish);
 double single_proc_time = (finish.tv_sec - start.tv_sec);
 single_proc_time += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
 clock_gettime(CLOCK_MONOTONIC, &start);
-res = max_word_in_file("/builds/naysudes/howeworks-technpark/project/tests/data/test1.txt");
+res = max_word_in_file("project/tests/data/test1.txt");
 clock_gettime(CLOCK_MONOTONIC, &finish);
 EXPECT_STR_EQ(paral_res, res);
 double multy_proc_time = (finish.tv_sec - start.tv_sec);
